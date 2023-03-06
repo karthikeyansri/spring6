58796600fb66ef27cb2f23fb3afc8c798641e228
karthikeyansri <karthikeyanqdx@gmail.com>
2023-03-05 22:52:40 -0500

    add Publisher

--- spring-6-webapp/src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
+++ spring-6-webapp/src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -39,26 +39,26 @@ public class BootstrapData implements CommandLineRunner {
         ddd.setIsbn("4315751764517");
         ddd.setPublisher(publisherSaved);
 
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
         Author ericSaved = authorRepository.save(eric);
         Book dddSaved = bookRepository.save(ddd);
 
         Author rod = new Author();
-        eric.setFirstName("Rod");
-        eric.setLastName("Johnson");
+        rod.setFirstName("Rod");
+        rod.setLastName("Johnson");
 
         Book noEJB = new Book();
         noEJB.setTitle("J2EE Development without EJB");
         noEJB.setIsbn("4315454764517");
         noEJB.setPublisher(publisherSaved);
 
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
         Author rodSaved = authorRepository.save(rod);
         Book noEJBSaved = bookRepository.save(noEJB);
-
-        rodSaved.getBooks().add(noEJBSaved);
-        ericSaved.getBooks().add(dddSaved);
-
-        authorRepository.save(ericSaved);
-        authorRepository.save(rodSaved);
 
         System.out.println("In BootstrapData;");
         System.out.println("Author Count = " + authorRepository.count());
